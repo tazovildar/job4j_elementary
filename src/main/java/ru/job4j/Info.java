2fafa8cbc0d6cfd5a964050aa7f33f6fe446bd79
tazovildar <tazovildar@mail.ru>
2021-10-29 12:30:35 +0500

    6. Checkstyle [#4123 #222030]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,7 +2,8 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("28.10.2021");
+        System.out.println("29.10.2021");
+        System.out.println("Задание выполнил");
     }
 
 }
