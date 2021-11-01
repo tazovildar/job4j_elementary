6915ff92669ba35954fa818edb41699b76b3e476
tazovildar <tazovildar@mail.ru>
2021-11-01 13:44:54 +0500

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,12 +1,9 @@
 package ru.job4j;
-
 public class Calculator {
-
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
-
     public static void main(String[] args) {
         Calculator.plus(1, 2);
         Calculator.plus(10, 11);
