3a839edd62edbe4c2b71e1857e80f493cd6ec1d2
tazovildar <tazovildar@mail.ru>
2021-11-02 14:05:12 +0500

    Исправление ArgMethod

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -1,6 +1,6 @@
-package ru.job4j;
+package ru.job4j.calculator;
 
-public class Argument {
+public class ArgMethod {
 
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
@@ -9,9 +9,9 @@ public class Argument {
     public static void main(String[] args) {
         String name = "Job4j";
         int age = 6;
-        Argument.hello(name, age);
-        Argument.hello(name, age);
-        Argument.hello(name, age);
-        Argument.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
     }
 }
